961bebe72be8e6a2ab16a802827637d14eed67b0
MSokol00 <sokolowski.marcin@me.com>
2017-01-25 11:07:45 +0100

    start handler

--- bot.py
+++ bot.py
@@ -1,10 +1,23 @@
 # resources
+from telegram.ext import Updater, CommandHandler
+import logging
 
 
 # configs
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+updater = Updater(token="308560134:AAGxr4JKMCj-vGKdIPZKvOT51Sf8UPW837o")
+dispatcher = updater.dispatcher
 
 
-# some code
+# command handlers
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
 
 # init
+if __name__ == "__main__":
+    updater.start_polling()
