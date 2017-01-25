fb48e249251cb31926c559af898c4e9e9964abcd
Marcin Sokolowski <MSokol00@iMac-Marcin.local>
2017-01-25 23:01:46 +0100

    changes in cron script

--- cron_startCMD.sh
+++ cron_startCMD.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-if [ -z "$(pidof python /home/MSokol00/Projects/telegram.rsvptime_bot/bot.py)" ]
+pidof python /home/MSokol00/Projects/telegram.rsvptime_bot/bot.py > /dev.null
+if [ $? -eq 0 ]
 then
+    echo Program Running
+else
     python /home/MSokol00/Projects/telegram.rsvptime_bot/cron_startCMD.sh
 fi
