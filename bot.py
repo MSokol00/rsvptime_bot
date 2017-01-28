8a579825b3a213d8fdf08747972c8df78ab09bea
Marcin Sokolowski <sokolowski.marcin@me.com>
2017-01-28 14:46:15 +0100

    make list

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # resources
 from telegram.ext import Updater, CommandHandler
+import mysql
 import logging
 
 
@@ -7,7 +8,7 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 updater = Updater(token="308560134:AAGxr4JKMCj-vGKdIPZKvOT51Sf8UPW837o")
-dispatcher = updater.dispatcher
+dispatcher = updater.dispatche
 
 
 # command handlers
@@ -18,13 +19,27 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 
-def krystian(bot, update):
+def krystian(bot, update, args):
     bot.sendMessage(chat_id=update.message.chat_id, text="Krystian is a pussy who uses cheats (like TriggerBot, WH and, "
                                                          "of course, AimBot) to pretend, that he's somewhat better in"
                                                          " CS:GO than rest of his team")
 
 krystian_handler = CommandHandler('krystian', krystian)
 dispatcher.add_handler(krystian_handler)
+
+
+def make(bot, update, args):
+    name = ''.join(args)
+    chat_id = update.message.chat_id
+    result = mysql.create_list(chat_id, name)
+    if result == True:
+        text = "List %s has been created succesfully!" % (name)
+    else:
+        text = "Ups! Something went wrong :( Your list has not been created. Please contact MSokol00 on telegram"
+    bot.sendMessage(chat_id=chat_id, text=text)
+
+make_handler = CommandHandler('make', make, pass_args=True)
+dispatcher.add_handler(make_handler)
 
 # init
 if __name__ == "__main__":
---
+++ mysql.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     #passwd="megajonhy",  # your password
+                     db="rsvptime_bot")        # name of the data base
+cur = db.cursor()
+
+
+## make list
+def create_list(chat_id, name):
+    sql = "INSERT INTO list (chat_id, name) VALUES (%s, %s)" % (chat_id, name)
+    try:
+        cur.execute(sql)
+        db.commit()
+        return True
+    except:
+        db.rollback()
+        return False
+
+
+
