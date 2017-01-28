f28ba092e3c6d63d88c8a7d205e28fe2c006a22e
Marcin Sokolowski <sokolowski.marcin@me.com>
2017-01-28 10:16:20 +0100

    final krystian show

--- bot.py
+++ bot.py
@@ -19,9 +19,11 @@ dispatcher.add_handler(start_handler)
 
 
 def krystian(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Krystian is pussy who uses cheats (like TriggerBot, WH and, of course, AimBot, to pretend, that he's somewhat better in CS than rest of his team")
+    bot.sendMessage(chat_id=update.message.chat_id, text="Krystian is a pussy who uses cheats (like TriggerBot, WH and, "
+                                                         "of course, AimBot) to pretend, that he's somewhat better in"
+                                                         " CS:GO than rest of his team")
 
-krystian_handler = CommandHandler('Krystian', krystian)
+krystian_handler = CommandHandler('krystian', krystian)
 dispatcher.add_handler(krystian_handler)
 
 # init
