c4359eae5e74c07dd1e068d6374111db6865c239
Marcin Sokolowski <sokolowski.marcin@me.com>
2017-01-28 15:42:27 +0100

    changes to permit only one list per chat

--- mysql.py
+++ mysql.py
@@ -5,10 +5,11 @@ db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
                      #passwd="megajonhy",  # your password
                      db="rsvptime_bot")        # name of the data base
-cur = db.cursor()
+
 
 def check_list_existence(chat_id):
-    sql = "SELECT 1 as 'result' FROM lists WHERE chat_id = %s" % (chat_id)
+    cur = db.cursor()
+    sql = "SELECT 'yes' as 'result' FROM lists WHERE chat_id = %s" % (chat_id)
     cur.execute(sql)
     ########
     print "####existence sql: \n"+str(sql)+"\n ________________"
@@ -17,24 +18,29 @@ def check_list_existence(chat_id):
     #######
     print "####result of existence: \n"
     print result
+    print result[1]
     print "____________________________"
     ########
-    if result[0] == '1': exist = True
+    if result[0] == 'yes': exist = True
     else: exist = False
     ########
     print exist
     ########
+    cur.close()
     return exist
 
 ## make list
 def create_list(chat_id, name):
+    cur = db.cursor()
     sql = "INSERT INTO lists (chat_id, name) VALUES (%s, '%s')" % (chat_id, name)
     try:
         cur.execute(sql)
         db.commit()
+        cur.close()
         return True
     except:
         db.rollback()
+        cur.close()
         return False
 
 
