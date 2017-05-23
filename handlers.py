c67b75bd8575b9c4b89c6d3bddd92b35d4000dce
MSokol00 <sokolowski.marcin@me.com>
2017-05-23 11:35:28 +0200

    time function debug

--- handlers.py
+++ handlers.py
@@ -40,6 +40,7 @@ def buildListText(list, status):
         if tuple[4] != 'NULL' and tuple[4] is not None:
             print "line 40: tuple[4]:", tuple[4]
             time = tuple[4]
+            print type(time)
             time_str = u" "+u"\u231A"+u" {}".format(time)
             print "line 44: time_str:", time_str
         else:
