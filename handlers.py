303c614d6c39d5a2b213322946e82416eb7aff1b
MSokol00 <sokolowski.marcin@me.com>
2017-05-23 11:30:43 +0200

    time function corrections

--- handlers.py
+++ handlers.py
@@ -40,8 +40,7 @@ def buildListText(list, status):
         if tuple[4] != 'NULL' and tuple[4] is not None:
             print "line 40: tuple[4]:", tuple[4]
             time = tuple[4]
-            print "line 42: time:", time
-            time_str = " "+u"\u231A"+" {:%H:%M}".format(time)
+            time_str = u" "+u"\u231A"+u" {:%H:%M}".format(time)
             print "line 44: time_str:", time_str
         else:
             time_str = ""
